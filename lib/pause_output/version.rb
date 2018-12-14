954ecb4c1d34c97ef05161107ee843fc5ea5562a
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-14 10:52:57 -0500

    First tests passing.

--- lib/pause_output/version.rb
+++ lib/pause_output/version.rb
@@ -1,3 +1,8 @@
+# coding: utf-8
+
+# Pause console output so that it can be read before scrolling off the screen.
 module PauseOutput
-  VERSION = "0.1.0"
+  VERSION = "0.1.0".freeze
+
+  DESCRIPTION = "pause_output: A simple facility to pause output on the console terminal.".freeze
 end
--- test/pause_output_test.rb
+++ test/pause_output_test.rb
@@ -1,11 +1,18 @@
-require "test_helper"
+require_relative '../lib/pause_output'
+gem              'minitest'
+require          'minitest/autorun'
+require          'minitest_visible'
 
 class PauseOutputTest < Minitest::Test
+
+  #Track mini-test progress.
+  include MinitestVisible
+
   def test_that_it_has_a_version_number
-    refute_nil ::PauseOutput::VERSION
+    refute_nil(::PauseOutput::VERSION)
+    assert(::PauseOutput::VERSION.frozen?)
+    assert(::PauseOutput::VERSION.is_a?(String))
+    assert(/\A\d+\.\d+\.\d+/ =~ ::PauseOutput::VERSION)
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
