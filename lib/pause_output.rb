98e0a17a2b88803dd8f8f2c8f36c19850819cf18
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-14 10:29:02 -0500

    Initial state from bundler.

---
+++ lib/pause_output.rb
@@ -0,0 +1,6 @@
+require "pause_output/version"
+
+module PauseOutput
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pause_output/version.rb
@@ -0,0 +1,3 @@
+module PauseOutput
+  VERSION = "0.1.0"
+end
