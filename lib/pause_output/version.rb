fffbdfb3e019c06a84699f668b4589e5c3198af7
PeterCamilleri <peter.c.camilleri@gmail.com>
2021-05-19 11:42:52 -0400

    Updated bundler dependency for next version

--- lib/pause_output/version.rb
+++ lib/pause_output/version.rb
@@ -2,7 +2,7 @@
 
 # Pause console output so that it can be read before scrolling off the screen.
 module PauseOutput
-  VERSION = "0.2.2".freeze
+  VERSION = "0.2.3".freeze
 
   DESCRIPTION = "pause_output: A simple facility to pause output on the console terminal.".freeze
 end
